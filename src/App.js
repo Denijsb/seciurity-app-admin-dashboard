ee785f18ce71377deedacb96e985ea45124a130a
denijs <denijs.balodis@inbox.lv>
2019-04-24 12:22:57 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button,
+} from 'reactstrap';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    id: null,
+    email: '',
+    password: ''
+  }
+
+  onEmailChange = event => {
+    this.setState({email: event.target.value})
+  }
+
+  onPasswordChange = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmit = () => {
+      axios.post(`//localhost:8080/units-api/sign-in`,
+          {email: this.state.email, password: this.state.password})
+          .then(response => console.log(response))
+  }
+
   render() {
+   const {id} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Container className="App">
+          <h2>{id ? id :'sign in'}</h2>
+          <Form className="form">
+            <Col>
+              <FormGroup>
+                <Label>Email</Label>
+                <Input
+                    type="email"
+                    name="email"
+                    id="exampleEmail"
+                    placeholder="myemail@email.com"
+                    onChange={this.onEmailChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                    type="password"
+                    name="password"
+                    id="examplePassword"
+                    placeholder="********"
+                    onChange={this.onPasswordChange}
+                />
+              </FormGroup>
+            </Col>
+            <Button onClick={this.onSubmit}>Submit</Button>
+          </Form>
+        </Container>
     );
   }
 }
